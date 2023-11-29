6a2015c3c0cc9e5f74ed79c610d8c91c8f694b54
zlxlwnfl@naver.com <zlxlwnfl@naver.com>
2023-11-29 20:22:48 +0900

    #3 필터(페이지, 이웃그룹) 구현

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 from PySide6.QtWidgets import *
 from PySide6.QtGui import *
+
 from ui.ui import Ui_MainWindow
 from qt_material import apply_stylesheet
 
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.webdriver import WebDriver
+
 import time
 import pyperclip
 
@@ -17,6 +20,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
         self.setupUi(self)
         self.logic = MainLogic()
 
+        self.pushButton_search.setDisabled(True)
+        self.pushButton_excelSave.setDisabled(True)
+
         # 로그인 관련 로직
         self.lineEdit_pw.setEchoMode(QLineEdit.EchoMode.Password)
         self.lineEdit_pw.returnPressed.connect(self.login)
@@ -25,7 +31,18 @@ class MainWindow(QMainWindow, Ui_MainWindow):
     def login(self):
         input_id = self.lineEdit_id.text()
         input_pw = self.lineEdit_pw.text()
-        self.logic.login(input_id, input_pw)
+
+        result = self.logic.login(input_id, input_pw)
+        if result is False:
+            return
+
+        self.pushButton_search.setEnabled(True)
+
+        default_page_data = self.logic.find_default_page_data()
+        self.lineEdit_startPage.setText(str(default_page_data[0]))
+        self.lineEdit_endPage.setText(str(default_page_data[1]))
+
+        self.comboBox_neighborGroup.addItems(self.logic.find_neighbor_group_list())
 
     def closeEvent(self, event: QCloseEvent):
         result = QMessageBox.question(
@@ -44,18 +61,22 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 # 메인 로직 클래스
 class MainLogic:
     def __init__(self):
-        self.web_browser = None
+        self.web_browser: WebDriver = None
 
-    def login(self, input_id, input_pw):
+    def naver_blog_main_url(self) -> str:
+        return 'https://section.blog.naver.com/BlogHome.naver'
+
+    def login(self, input_id, input_pw) -> bool:
         # 네이버 블로그 페이지 로딩
-        self.web_browser = webdriver.Chrome()
-        self.web_browser.get('https://section.blog.naver.com/BlogHome.naver')
+        if self.web_browser is None:
+            self.web_browser = webdriver.Chrome()
+        self.web_browser.get(self.naver_blog_main_url())
         time.sleep(2)
 
         # 로그인 상태가 아니라면 네이버 로그인 화면으로 이동
         login_button = self.web_browser.find_element(By.CLASS_NAME, "login_button")
         if login_button is None:
-            return
+            return True
         else:
             login_button.click()
 
@@ -69,6 +90,50 @@ class MainLogic:
         time.sleep(2)
 
         self.web_browser.find_element(value="log.login").click()
+        time.sleep(2)
+        if self.naver_blog_main_url() in self.web_browser.current_url:
+            return True
+        else:
+            return False
+
+    def find_default_page_data(self) -> []: # [start_page, end_page]
+        page_navigation = self.web_browser.find_element(
+            by=By.XPATH,
+            value='//*[@id="content"]/section/div[3]/div',
+        )
+        if page_navigation is None:
+            return [1, 1]
+
+        pages = page_navigation.find_elements(
+            by=By.TAG_NAME,
+            value='span'
+        )
+        start_page = 1
+        end_page = len(pages)
+
+        return [start_page, end_page]
+
+    def find_neighbor_group_list(self) -> []:
+        self.web_browser.find_element(
+            by=By.XPATH,
+            value='//*[@id="content"]/section/div[1]/div/div/a/i',
+        ).click()
+        time.sleep(1)
+
+        neighbor_group_elements_list = self.web_browser.find_element(
+            by=By.XPATH,
+            value='//*[@id="content"]/section/div[1]/div/div/div',
+        ).find_elements(
+            by=By.TAG_NAME,
+            value='a',
+        )
+        neighbor_group_list = list(
+            map(
+                lambda x: x.text,
+                neighbor_group_elements_list
+            )
+        )
+        return neighbor_group_list
 
     def window_close(self):
         if self.web_browser is not None:
@@ -78,7 +143,7 @@ class MainLogic:
 # Qt 애플리케이션 생성
 app = QApplication()
 window = MainWindow()
-apply_stylesheet(app, theme='dark_amber.xml')
+apply_stylesheet(app, theme='light_blue.xml')
 
 # 애플리케이션 실행
 window.show()
