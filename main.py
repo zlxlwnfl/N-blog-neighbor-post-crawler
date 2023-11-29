3728f725f1a07928be0a76b9876efa51af80c0f4
zlxlwnfl@naver.com <zlxlwnfl@naver.com>
2023-11-29 18:11:52 +0900

    리팩토링

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
-from PySide6 import QtWidgets, QtGui
+from PySide6.QtWidgets import *
+from PySide6.QtGui import *
 from ui.ui import Ui_MainWindow
 from qt_material import apply_stylesheet
 
@@ -10,32 +11,31 @@ import pyperclip
 
 
 # 메인 ui 클래스
-class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
+class MainWindow(QMainWindow, Ui_MainWindow):
     def __init__(self):
         super().__init__()
         self.setupUi(self)
-
         self.logic = MainLogic()
 
         # 로그인 관련 로직
-        self.lineEdit_pw.setEchoMode(QtWidgets.QLineEdit.EchoMode.Password)
+        self.lineEdit_pw.setEchoMode(QLineEdit.EchoMode.Password)
         self.lineEdit_pw.returnPressed.connect(self.login)
         self.pushButton_login.clicked.connect(self.login)
 
     def login(self):
-        inputId = self.lineEdit_id.text()
-        inputPw = self.lineEdit_pw.text()
+        input_id = self.lineEdit_id.text()
+        input_pw = self.lineEdit_pw.text()
+        self.logic.login(input_id, input_pw)
 
-        self.logic.login(inputId, inputPw)
-
-    def closeEvent(self, event: QtGui.QCloseEvent):
-        result = QtWidgets.QMessageBox.question(self,
-                                                "종료 확인",
-                                                "종료하시겠습니까?",
-                                                QtWidgets.QMessageBox.StandardButton.Yes|
-                                                QtWidgets.QMessageBox.StandardButton.No)
-        if result is QtWidgets.QMessageBox.StandardButton.Yes:
-            self.logic.windowClose()
+    def closeEvent(self, event: QCloseEvent):
+        result = QMessageBox.question(
+            self,
+            "종료 확인",
+            "종료하시겠습니까?",
+            QMessageBox.StandardButton.Yes | QMessageBox.StandardButton.No
+        )
+        if result == QMessageBox.StandardButton.Yes:
+            self.logic.window_close()
             event.accept()
         else:
             event.ignore()
@@ -43,39 +43,40 @@ class MainWindow(QtWidgets.QMainWindow, Ui_MainWindow):
 
 # 메인 로직 클래스
 class MainLogic:
-    def login(self, inputId, inputPw):
+    def __init__(self):
+        self.driver = None
+
+    def login(self, input_id, input_pw):
         # 네이버 블로그 페이지 로딩
         self.driver = webdriver.Chrome()
         self.driver.get('https://section.blog.naver.com/BlogHome.naver')
         time.sleep(2)
 
         # 로그인 상태가 아니라면 네이버 로그인 화면으로 이동
-        loginButton = self.driver.find_element(By.CLASS_NAME, "login_button")
-        if loginButton is None:
+        login_button = self.driver.find_element(By.CLASS_NAME, "login_button")
+        if login_button is None:
             return
         else:
-            loginButton.click()
+            login_button.click()
 
         # 캡챠 우회 위해 복붙+딜레이 활용
-        pyperclip.copy(inputId)
+        pyperclip.copy(input_id)
         self.driver.find_element(value="id").send_keys(Keys.CONTROL, 'v')
         time.sleep(2)
 
-        pyperclip.copy(inputPw)
+        pyperclip.copy(input_pw)
         self.driver.find_element(value="pw").send_keys(Keys.CONTROL, 'v')
         time.sleep(2)
 
         self.driver.find_element(value="log.login").click()
 
-    def windowClose(self):
-        if self.driver is None:
-            return
-        else:
+    def window_close(self):
+        if self.driver is not None:
             self.driver.close()
 
 
 # Qt 애플리케이션 생성
-app = QtWidgets.QApplication()
+app = QApplication()
 window = MainWindow()
 apply_stylesheet(app, theme='dark_amber.xml')
 
