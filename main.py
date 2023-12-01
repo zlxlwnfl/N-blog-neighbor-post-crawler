19ddf535734b24044286c8df4a14807c4ace4143
zlxlwnfl@naver.com <zlxlwnfl@naver.com>
2023-12-01 20:46:21 +0900

    #3 팝업 뜨면 끄는 로직 추가

--- main.py
+++ main.py
@@ -11,6 +11,7 @@ from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.action_chains import ActionChains
 from selenium.common.exceptions import NoSuchElementException
 
 import time
@@ -79,6 +80,9 @@ class MainWindow(QMainWindow, Ui_MainWindow):
                 QMessageBox.StandardButton.Ok
             )
 
+        # TODO: 팝업 잘 지우는지 확인 필요
+        self.logic.close_floating_popup()
+
         # 이웃그룹 설정
         self.logic.set_neighbor_group(self.comboBox_neighborGroup.currentIndex())
         print(f'currentIndex {self.comboBox_neighborGroup.currentIndex()}')
@@ -131,6 +135,7 @@ class MainWindow(QMainWindow, Ui_MainWindow):
 class MainLogic:
     def __init__(self):
         self.web_browser: WebDriver = None
+        self.actions: ActionChains = None
 
         # 네이버 블로그 메인 url 쿼리 파싱
         naver_blog_main_url_query = urlparse(self.naver_blog_main_url()).query
@@ -143,6 +148,7 @@ class MainLogic:
         # 네이버 블로그 페이지 로딩
         if self.web_browser is None:
             self.web_browser = webdriver.Chrome()
+            self.actions = ActionChains(self.web_browser)
         self.web_browser.get(self.naver_blog_main_url())
         time.sleep(2)
 
@@ -237,6 +243,7 @@ class MainLogic:
 
         for target_page in range(start_page, end_page+1):
             self.web_browser.get(self.get_naver_blog_main_target_page_url(target_page, neighbor_group_id))
+            self.close_floating_popup()
             time.sleep(1)
 
             posts = None
@@ -310,6 +317,22 @@ class MainLogic:
 
         print(result_list)
         return result_list
+
+    def close_floating_popup(self):
+        try:
+            popup = self.web_browser.find_element(
+                by=By.ID,
+                value='floatingda_home'
+            )
+            self.actions.move_to_element(popup).perform()
+            time.sleep(0.3)
+
+            self.web_browser.find_element(
+                by=By.XPATH,
+                value='//*[@id="floatingda_home"]/div/button'
+            ).click()
+        except NoSuchElementException:
+            pass
 
     def window_close(self):
         if self.web_browser is not None:
